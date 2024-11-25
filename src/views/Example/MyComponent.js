3feb20db4843f3e35499bc3b9ba018f4e5da3bd9
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2024-11-25 23:17:51 +0700

    #8

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,37 +1,50 @@
-import React from 'react';
+import React from "react";
 
 class MyComponent extends React.Component {
-
     state = {
-        name: 'Long',
-        age: 12
-    }
+        name: "",
+        age: 12,
+    };
 
     handleChangeName = (event) => {
+        console.log(
+            event.target.value,
+            "event target: ",
+            event.target,
+            "event object:",
+            event
+        );
         this.setState({
-            name: event.target.value
-        })
-    }
+            name: event.target.value,
+        });
+    };
+
+    handleClickButton = () => {
+        console.log("click the button");
+        alert("click me");
+    };
 
     render() {
+        console.log(">>>>> call render: ", this.state);
         return (
             <>
-                <div className='first'>
-                    {console.log('My name is :', this.state.name)}
-                    <input value={this.state.name} type='text'
+                <div className="first">
+                    {console.log("My name is :", this.state.name)}
+                    <input
+                        value={this.state.name}
+                        type="text"
                         onChange={(event) => this.handleChangeName(event)}
                     />
                     My name is {this.state.name}
                 </div>
-                <div className='second'>
-                    My age is {this.state.age}
+                <div className="second">My age is {this.state.age}</div>
+                <div className="third">
+                    <button onClick={() => this.handleClickButton()}>
+                        Click me
+                    </button>
                 </div>
             </>
-
-
-
-        )
-
+        );
     }
 }
 
