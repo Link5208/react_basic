24611b474b696d31e054d4d42a13f9339411ded8
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2024-12-06 10:20:07 +0700

    #end

--- src/stores/reducers/rootReducer.js
+++ src/stores/reducers/rootReducer.js
@@ -1,13 +1,31 @@
 const initState = {
   users: [
     { id: 1, name: "Long" },
-    {
-      id: 2,
-      name: "Link",
-    },
+    { id: 2, name: "Link" },
+    { id: 3, name: "Hoang" },
   ],
+  posts: [],
 };
-const rootReducer = (state, action) => {
+const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "DELETE_USER":
+      console.log("run into delete user:", action);
+      let users = state.users;
+      users = users.filter((item) => item.id !== action.payload.id);
+      return { ...state, users };
+      break;
+
+    case "CREATE_USER":
+      let id = Math.floor(Math.random() * 1001);
+      let user = { id: id, name: `random ${id}` };
+      return {
+        ...state,
+        users: [...state.users, user],
+      };
+      break;
+    default:
+      break;
+  }
   return state;
 };
 export default rootReducer;
